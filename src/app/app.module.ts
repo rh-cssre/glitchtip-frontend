ddadf4c0c9f3cb01cf8776a430d2034b4a8bc01c
Emily <emily@burkesoftware.com>
2019-11-20 17:48:00 -0500

    Fixed sharedmodule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,15 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatSliderModule } from "@angular/material/slider";
 import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
-  declarations: [AppComponent, SharedModule],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
