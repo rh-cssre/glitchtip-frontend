78d57cab5bb5f1f203c5346f7ff856ee9e904614
Brendan <me@brendanberkley.com>
2019-11-20 17:57:22 -0500

    bloop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 const routes: Routes = [
-  { path: '', component: AppComponent, pathMatch: 'full' },
   {
     path: "issues",
     loadChildren: () =>
