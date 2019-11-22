81b32a4e3207b396be46648b54ff7941a16fd9db
David Burke <david@burkesoftware.com>
2019-11-21 21:32:10 -0500

    login works sort of

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AuthService } from "../api/auth/auth.service";
 
 @Component({
   selector: "app-login",
@@ -6,7 +9,35 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent {
+  loading = false;
+  error: string;
+  form = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(8)
+    ])
+  });
+
+  constructor(private authService: AuthService, private router: Router) {}
+
   onSubmit() {
-    console.log("Nice button poosh");
+    if (this.form.valid) {
+      this.loading = true;
+      this.authService
+        .login(this.form.value.email, this.form.value.password)
+        .subscribe(
+          data => this.router.navigate([""]),
+          err => {
+            this.loading = false;
+            console.log(err);
+            if (err.status === 400 && err.error.non_field_errors) {
+              this.error = err.error.non_field_errors;
+            } else {
+              this.error = "Error";
+            }
+          }
+        );
+    }
   }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { LoginComponent } from "./login.component";
-import { LoginRoutingModule } from "./login-routing.module";
 import { MatInputModule } from "@angular/material/input";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { ReactiveFormsModule } from "@angular/forms";
+import { LoginComponent } from "./login.component";
+import { LoginRoutingModule } from "./login-routing.module";
+import { AuthModule } from "../api/auth/auth.module";
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
+    ReactiveFormsModule,
+    AuthModule,
     MatInputModule,
     MatCheckboxModule,
     MatButtonModule,
