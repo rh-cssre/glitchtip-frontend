5dfeb2fe805d6a8a1766582dbb3d69c507074ea4
Emily <emily@burkesoftware.com>
2019-12-05 15:22:49 -0500

    Set up project list view

--- src/app/settings/projects/projects.component.ts
+++ src/app/settings/projects/projects.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProjectsService } from "../../api/projects/projects.service";
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  selector: "app-projects",
+  templateUrl: "./projects.component.html",
+  styleUrls: ["./projects.component.scss"]
 })
 export class ProjectsComponent implements OnInit {
-
-  constructor() { }
+  projects: any;
+  constructor(private projectsService: ProjectsService) {}
 
   ngOnInit() {
+    this.projectsService.retrieveProjects().subscribe(projects => {
+      this.projects = projects;
+    });
   }
-
 }
