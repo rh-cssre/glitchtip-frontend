0b389868d6e60bf2dfabc8a5b8d0aae425aa8d42
Brendan <brendan@burkesoftware.com>
2019-12-17 12:02:34 -0500

    WIP newsletter stuff

--- projects/marketing/src/app/app.module.ts
+++ projects/marketing/src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppComponent } from "./app.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), MatToolbarModule, MatButtonModule, MatCardModule],
+  imports: [
+    BrowserModule.withServerTransition({ appId: "serverApp" }),
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
