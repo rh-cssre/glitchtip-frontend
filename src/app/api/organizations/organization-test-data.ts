40bb4f0ae5a8f2477a93b9806f1fb3d4d17193e9
Emily <emily@burkesoftware.com>
2019-12-20 14:19:08 -0500

    Added test data and made tests for initail state

---
+++ src/app/api/organizations/organization-test-data.ts
@@ -0,0 +1,36 @@
+import { Organization } from "./organizations.interface";
+
+export const organizaions: Organization[] = [
+  {
+    id: 2,
+    name: "test org",
+    slug: "test-org",
+    dateCreated: "2019-12-20T14:50:31.641549Z",
+    status: {
+      id: "active",
+      name: "active"
+    },
+    avatar: {
+      avatarType: "",
+      avatarUuid: null
+    },
+    isEarlyAdopter: false,
+    require2FA: false
+  },
+  {
+    id: 1,
+    name: "prince ali",
+    slug: "prince-ali",
+    dateCreated: "2019-12-20T14:49:28.631459Z",
+    status: {
+      id: "active",
+      name: "active"
+    },
+    avatar: {
+      avatarType: "",
+      avatarUuid: null
+    },
+    isEarlyAdopter: false,
+    require2FA: false
+  }
+];
--- src/app/api/organizations/organizations.service.spec.ts
+++ src/app/api/organizations/organizations.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from "@angular/core/testing";
 
 import { OrganizationsService } from "./organizations.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { Organization } from "./organizations.interface";
 
 describe("OrganizationsService", () => {
   beforeEach(() =>
@@ -11,5 +12,19 @@ describe("OrganizationsService", () => {
   it("should be created", () => {
     const service: OrganizationsService = TestBed.get(OrganizationsService);
     expect(service).toBeTruthy();
+  });
+
+  fit("Initial organizations", () => {
+    const service: OrganizationsService = TestBed.get(OrganizationsService);
+    service.organziations$.subscribe((organizations: Organization[]) => {
+      expect(organizations).toEqual([]);
+    });
+  });
+
+  fit("Initial active organization", () => {
+    const service: OrganizationsService = TestBed.get(OrganizationsService);
+    service.activeOrganization$.subscribe(activeOrganization => {
+      expect(activeOrganization).toBe(null);
+    });
   });
 });
