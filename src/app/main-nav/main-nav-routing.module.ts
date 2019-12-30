f07de2b7b0bc780dffc275a3a7b36277eaa38e07
Emily <emily@burkesoftware.com>
2019-12-30 12:43:00 -0500

    Fixed router working on first click bug

--- src/app/main-nav/main-nav-routing.module.ts
+++ src/app/main-nav/main-nav-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainNavComponent } from "./main-nav/main-nav.component";
+import { IsLoggedInGuard } from "../guards/is-logged-in.guard";
 
 const routes: Routes = [
   {
@@ -11,24 +12,29 @@ const routes: Routes = [
         path: "",
         loadChildren: () =>
           import("../home/home.module").then(m => m.HomeModule),
-        pathMatch: "full"
+        pathMatch: "full",
+        canActivate: [IsLoggedInGuard]
       },
       {
         path: "settings",
         loadChildren: () =>
-          import("../settings/settings.module").then(m => m.SettingsModule)
+          import("../settings/settings.module").then(m => m.SettingsModule),
+        canActivate: [IsLoggedInGuard]
       },
+
       {
         path: "organizations/:org-slug/issues",
         loadChildren: () =>
-          import("../issues/issues.module").then(m => m.IssuesModule)
+          import("../issues/issues.module").then(m => m.IssuesModule),
+        canActivate: [IsLoggedInGuard]
       },
       {
         path: "organizations/new",
         loadChildren: () =>
           import("../new-organization/new-organization.module").then(
             m => m.NewOrganizationModule
-          )
+          ),
+        canActivate: [IsLoggedInGuard]
       }
     ]
   }
