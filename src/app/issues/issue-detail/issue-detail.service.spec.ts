0fa873dc6f29173301b712744b5e0673585222d7
David Burke <david@burkesoftware.com>
2019-12-31 10:59:28 -0500

    Added issue detail spec

--- src/app/issues/issue-detail/issue-detail.service.spec.ts
+++ src/app/issues/issue-detail/issue-detail.service.spec.ts
@@ -1,15 +1,33 @@
 import { TestBed } from "@angular/core/testing";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
 import { IssueDetailService } from "./issue-detail.service";
+import { IssueDetail } from "../interfaces";
+import { sampleIssue } from "../issue-list-item/sample-data";
 
 describe("IssueDetailService", () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] })
-  );
+  let httpTestingController: HttpTestingController;
+  let service: IssueDetailService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(IssueDetailService);
+  });
 
   it("should be created", () => {
-    const service: IssueDetailService = TestBed.get(IssueDetailService);
     expect(service).toBeTruthy();
+  });
+
+  it("should retieve issue detail", () => {
+    const testData: IssueDetail = sampleIssue;
+    service.retrieveIssue(testData.id).toPromise();
+    const req = httpTestingController.expectOne(
+      `/api/0/issues/${testData.id}/`
+    );
+    req.flush(testData);
+    service.issue$.subscribe(issue => expect(issue).toEqual(testData));
   });
 });
