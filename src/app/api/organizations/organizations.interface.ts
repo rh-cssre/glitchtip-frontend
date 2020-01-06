ba7ba3aa758950d2cd3130c7efaebb1a4029b78a
Emily <emily@burkesoftware.com>
2020-01-06 11:43:10 -0500

    Started on org detail interface

--- src/app/api/organizations/organizations.interface.ts
+++ src/app/api/organizations/organizations.interface.ts
@@ -13,7 +13,17 @@ export interface Organization extends OrganizationNew {
 }
 
 // tslint:disable-next-line:no-empty-interface
-export interface OrganizationDetail extends Organization {}
+export interface OrganizationDetail extends Organization {
+  access: string[];
+  allowSharedIssues: boolean;
+  availableRoles: IAvailableRoles[];
+  dataScrubber: boolean;
+  dataScrubberDefaults: boolean;
+  defaultRole: string;
+  enhancedPrivacy: boolean;
+  experiments: object;
+  features: IFeatures[];
+}
 
 interface IStatus {
   id: string;
@@ -24,3 +34,22 @@ interface IAvatar {
   avatarType: string;
   avatarUuid: string | null;
 }
+
+interface IAvailableRoles {
+  id: AvailableRolesId;
+  name: AvailableRolesName;
+}
+
+type AvailableRolesId = "member" | "admin" | "manager" | "owner";
+
+type AvailableRolesName = "Member" | "Admin" | "Manager" | "Owner";
+
+type IFeatures = "new-teams" &
+  "shared-issues" &
+  "new-issues-ui" &
+  "repos" &
+  "open-membership" &
+  "invite-members" &
+  "sso-saml2" &
+  "sso-basic" &
+  "suggested-commits";
