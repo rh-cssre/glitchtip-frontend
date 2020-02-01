dfb2762c57cf737eb108ef5d52daad99e04e9d33
David Burke <david@burkesoftware.com>
2020-02-01 12:31:13 -0500

    Fix tests

--- src/app/api/oauth/oauth.service.spec.ts
+++ src/app/api/oauth/oauth.service.spec.ts
@@ -1,9 +1,22 @@
 import { TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { MatSnackBarModule } from "@angular/material";
 
 import { GlitchTipOAuthService } from "./oauth.service";
+import { OAuthModule } from "angular-oauth2-oidc";
 
 describe("OauthService", () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatSnackBarModule,
+        OAuthModule.forRoot()
+      ]
+    })
+  );
 
   it("should be created", () => {
     const service: GlitchTipOAuthService = TestBed.get(GlitchTipOAuthService);
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,14 +1,13 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { ReactiveFormsModule } from "@angular/forms";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatCardModule } from "@angular/material/card";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { RouterTestingModule } from "@angular/router/testing";
-import { MatInputModule } from "@angular/material/input";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { EMPTY } from "rxjs";
+import { OAuthModule } from "angular-oauth2-oidc";
 import { LoginService } from "./login.service";
 import { LoginComponent } from "./login.component";
+import { MaterialModule } from "../shared/material.module";
 
 const authServiceSpy = jasmine.createSpyObj("LoginService", ["login"]);
 
@@ -22,11 +21,10 @@ describe("LoginComponent", () => {
       imports: [
         NoopAnimationsModule,
         ReactiveFormsModule,
-        MatInputModule,
-        MatFormFieldModule,
-        MatCheckboxModule,
-        MatCardModule,
-        RouterTestingModule
+        MaterialModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        OAuthModule.forRoot()
       ],
       providers: [{ provide: LoginService, useValue: authServiceSpy }]
     }).compileComponents();
