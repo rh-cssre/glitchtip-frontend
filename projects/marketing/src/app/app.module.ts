26f154be0dc75dd00102c64a1e26a59cfdc26ffa
Brendan <brendan@burkesoftware.com>
2020-01-31 20:58:25 -0500

    not great, but done-ish

--- projects/marketing/src/app/app.module.ts
+++ projects/marketing/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: "serverApp" }),
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/marketing/src/app/legal/legal.module.ts
+++ projects/marketing/src/app/legal/legal.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PrivacyComponent } from "./privacy/privacy.component";
 import { LegalRoutingModule } from "./legal-routing.module";
+import { MatCardModule } from "@angular/material";
 
 @NgModule({
   declarations: [PrivacyComponent],
-  imports: [CommonModule, LegalRoutingModule]
+  imports: [CommonModule, LegalRoutingModule, MatCardModule]
 })
 export class LegalModule {}
