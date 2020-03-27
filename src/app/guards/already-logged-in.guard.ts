b0b54f9a292e86d44842ea6afe01eec3706bc32f
David Burke <david@burkesoftware.com>
2020-03-27 17:00:31 -0400

    Fixes #21 added guard to prevent logged in user from seeing login page

---
+++ src/app/guards/already-logged-in.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { AuthService } from "../api/auth/auth.service";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AlreadyLoggedInGuard implements CanActivate {
+  isLoggedIn: boolean;
+
+  constructor(private auth: AuthService, private router: Router) {
+    this.auth.isLoggedIn.subscribe(
+      isLoggedIn => (this.isLoggedIn = isLoggedIn)
+    );
+  }
+  canActivate() {
+    if (this.isLoggedIn) {
+      this.router.navigate([""]);
+      return false;
+    }
+    return true;
+  }
+}
