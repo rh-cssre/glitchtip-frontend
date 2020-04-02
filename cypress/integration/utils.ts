560b989b08fb99f714d2e65c7458c85562f7781f
David Burke <david@burkesoftware.com>
2020-04-02 01:11:31 +0000

    Cypress ci

--- cypress/integration/utils.ts
+++ cypress/integration/utils.ts
@@ -1,20 +1,12 @@
 import { user } from "../fixtures/users";
 
-function getBackendUrl() {
-  let backendBaseURL = Cypress.env("backendBaseURL");
-  if (!backendBaseURL) {
-    backendBaseURL = "";
-  }
-  return backendBaseURL;
-}
-
 export function seedBackend() {
-  const url = getBackendUrl() + "/api/test/seed/";
+  const url = "/api/test/seed/";
   cy.request("POST", url);
 }
 
 export function requestLogin() {
-  const url = getBackendUrl() + "/rest-auth/login/";
+  const url = "/rest-auth/login/";
   cy.request("POST", url, {
     email: user.email,
     password: user.password
