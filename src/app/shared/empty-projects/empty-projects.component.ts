32cfef7b7574209b99b00fe98104563b2d96fdea
Brendan <brendan@burkesoftware.com>
2020-05-15 09:59:18 -0400

    #27 empty state in project settings

---
+++ src/app/shared/empty-projects/empty-projects.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+
+@Component({
+  selector: "app-empty-projects",
+  templateUrl: "./empty-projects.component.html",
+  styleUrls: ["./empty-projects.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmptyProjectsComponent {}
