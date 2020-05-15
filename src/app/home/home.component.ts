cfa686be151e1d56a4a33c20b39e97c80849364a
Brendan <brendan@burkesoftware.com>
2020-05-15 15:40:40 -0400

    #27 tweak home component logic to use active org projects only

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, ChangeDetectionStrategy, OnInit } from "@angular/core";
 import { OrganizationsService } from "../api/organizations/organizations.service";
 import { ProjectsService } from "../api/projects/projects.service";
+import { combineLatest } from "rxjs";
+import { filter, map } from "rxjs/operators";
 
 @Component({
   selector: "app-home",
@@ -12,6 +14,26 @@ export class HomeComponent implements OnInit {
   activeOrganizationDetail$ = this.organizationsService
     .activeOrganizationDetail$;
   projects$ = this.projectsService.projects$;
+
+  /**
+   * this.organizationsService.activeOrganizationProjects$ will give similar
+   * info, but I don't think it has as much information about the projects.
+   * I had a choice between using it or ditching this.projectsService.projects$,
+   * and I kept the latter since that's what's here.
+   */
+  activeOrganizationProjects$ = combineLatest([
+    this.activeOrganizationDetail$,
+    this.projects$,
+  ]).pipe(
+    map(([activeOrgDetail, projects]) => {
+      if (!activeOrgDetail || !projects) {
+        return [];
+      }
+      return projects.filter(
+        (project) => project.organization.slug === activeOrgDetail.slug
+      );
+    })
+  );
 
   constructor(
     private organizationsService: OrganizationsService,
