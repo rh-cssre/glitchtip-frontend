5cb0c3a0f12a6d8e98b3668179a59ea831ae1115
Emily <emily@burkesoftware.com>
2020-06-01 09:51:55 -0400

    Fix tests

--- src/app/settings/subscription/stripe.service.spec.ts
+++ src/app/settings/subscription/stripe.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from "@angular/core/testing";
 
 import { StripeService } from "./stripe.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { MatomoModule } from "ngx-matomo";
 
 describe("StripeService", () => {
   let service: StripeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule, MatomoModule],
+    });
     service = TestBed.inject(StripeService);
   });
 
--- src/app/settings/subscription/subscription.component.spec.ts
+++ src/app/settings/subscription/subscription.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { SubscriptionComponent } from "./subscription.component";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { MatomoModule } from "ngx-matomo";
 
 describe("SubscriptionComponent", () => {
   let component: SubscriptionComponent;
@@ -11,7 +12,7 @@ describe("SubscriptionComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SubscriptionComponent],
-      imports: [HttpClientTestingModule, RouterTestingModule]
+      imports: [HttpClientTestingModule, RouterTestingModule, MatomoModule],
     }).compileComponents();
   }));
 
