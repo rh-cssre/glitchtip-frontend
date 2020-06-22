c005ec498901affc311cd1f4f24540c6dd39ac1a
Brendan <brendan@burkesoftware.com>
2020-06-22 14:43:00 -0400

    add sample data

---
+++ src/app/api/user/sample-email-address-data.ts
@@ -0,0 +1,27 @@
+export const sampleEmailAddressData = [
+  {
+    isPrimary: false,
+    email: "aa@example.com",
+    isVerified: true,
+  },
+  {
+    isPrimary: false,
+    email: "ab@example.com",
+    isVerified: true,
+  },
+  {
+    isPrimary: false,
+    email: "test@example.com",
+    isVerified: false,
+  },
+  {
+    isPrimary: true,
+    email: "primaryEmailAddress@example.com",
+    isVerified: true,
+  },
+  {
+    isPrimary: false,
+    email: "hello@example.com",
+    isVerified: false,
+  },
+];
