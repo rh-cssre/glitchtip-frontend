8d61113ca952c3fa2df43209f54d686fe7b7a110
Emily Jensen <emily@burkesoftware.com>
2020-06-24 15:21:58 +0000

    Improve Use of Cards

--- src/app/shared/shared.stories.ts
+++ src/app/shared/shared.stories.ts
@@ -6,10 +6,9 @@ import { withKnobs, boolean, text } from "@storybook/addon-knobs";
 
 import { MaterialModule } from "src/app/shared/material.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ProjectCardComponent } from "./card/project-card/project-card.component";
+import { ProjectCardComponent } from "./project-card/project-card.component";
 import { EntryDataComponent } from "./entry-data/entry-data.component";
 import { LoadingButtonComponent } from "./loading-button/loading-button.component";
-import { CardComponent } from "./card/card.component";
 
 export default {
   title: "Shared",
@@ -22,38 +21,9 @@ export default {
         RouterTestingModule,
         BrowserAnimationsModule,
       ],
-      declarations: [CardComponent],
     }),
     withKnobs,
   ],
-};
-
-export const card = () => {
-  return {
-    template: `
-    <app-card
-      title="Input title"
-    >
-      <div content>
-        This is the content section of app-card.
-        <ul>
-          <li>Pass in a "title"</li>
-          <li>The card is fully clickable and accepts a routerLink to "cardLink" and query params to "cardLinkQueryParams"</li>
-          <li>Content can be added to the card by adding a "content" selector to the tag wrapping that section</li>
-          <li>Add actions to the bottom of the card by adding an "actions" selector to the tag wrapping your actions.</li>
-        </ul>
-      </div>
-      <ng-container actions>
-        <button mat-stroked-button color="primary">Use the "actions" selector for actions</button>
-        <button mat-flat-button color="primary">they are always flexed with space-between</button>
-      </ng-container>
-    </app-card>
-    `,
-  };
-};
-
-card.story = {
-  parameters: {},
 };
 
 export const projectCard = () => {
