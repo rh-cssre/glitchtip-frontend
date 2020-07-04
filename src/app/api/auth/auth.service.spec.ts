eea1aab22a387d405a7efa5b9ad4939f208aeed1
David Burke <david@burkesoftware.com>
2020-07-04 19:35:18 -0400

    Fix spec

--- src/app/api/auth/auth.service.spec.ts
+++ src/app/api/auth/auth.service.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 import { AuthService } from "./auth.service";
 
 describe("AuthService", () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, MatSnackBarModule]
+      imports: [
+        RouterTestingModule,
+        MatSnackBarModule,
+        HttpClientTestingModule,
+      ],
     })
   );
 
