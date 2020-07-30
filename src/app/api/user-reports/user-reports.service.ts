7c3052f6bffde0e71101111e2ed26d81ce919dfc
Brendan <brendan@burkesoftware.com>
2020-07-30 16:31:33 -0400

    pagination for user reports

--- src/app/api/user-reports/user-reports.service.ts
+++ src/app/api/user-reports/user-reports.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { tap, catchError, map } from "rxjs/operators";
 import { EMPTY, BehaviorSubject } from "rxjs";
 import { baseUrl } from "../../constants";
 import { UserReport } from "./user-reports.interfaces";
+import { urlParamsToObject } from "src/app/issues/utils";
 
 interface UserReportsState {
-  reports: UserReport[];
+  reports: UserReport[] | null;
+  nextPage: string | null;
+  previousPage: string | null;
   loading: { reports: boolean };
   errors: { reports: string | null };
 }
 
 const initialState: UserReportsState = {
-  reports: [],
+  reports: null,
+  nextPage: null,
+  previousPage: null,
   loading: { reports: false },
   errors: { reports: null },
 };
@@ -30,17 +35,32 @@ export class UserReportsService {
   reports$ = this.getState$.pipe(map((state) => state.reports));
   loadingReports$ = this.getState$.pipe(map((state) => state.loading.reports));
   errorsReports$ = this.getState$.pipe(map((state) => state.errors.reports));
+  hasNextPage$ = this.getState$.pipe(map((state) => state.nextPage !== null));
+  hasPreviousPage$ = this.getState$.pipe(
+    map((state) => state.previousPage !== null)
+  );
+  nextPageParams$ = this.getState$.pipe(
+    map((state) => urlParamsToObject(state.nextPage))
+  );
+  previousPageParams$ = this.getState$.pipe(
+    map((state) => urlParamsToObject(state.previousPage))
+  );
 
   constructor(private http: HttpClient) {}
 
-  getReportsForIssue(issue: number) {
+  getReportsForIssue(issue: number, cursor: string | undefined) {
     this.setLoadingReports(true);
-    this.getIssueReports(issue)
+    this.resetUserReports();
+    this.getIssueReports(issue, cursor)
       .pipe(
         tap((response) => {
           this.setLoadingReports(false);
           this.resetErrorsReports();
-          this.setReports(response);
+          const linkHeader = response.headers.get("link");
+          if (response.body && linkHeader) {
+            this.setReports(response.body);
+            this.setPagination(linkHeader);
+          }
         }),
         catchError((error) => {
           this.setLoadingReports(false);
@@ -53,9 +73,14 @@ export class UserReportsService {
       .subscribe();
   }
 
-  private getIssueReports(issueId: number) {
+  private getIssueReports(issueId: number, cursor: string | undefined) {
+    let httpParams = new HttpParams();
+    if (cursor) {
+      httpParams = httpParams.set("cursor", cursor);
+    }
     return this.http.get<UserReport[]>(
-      `${this.issuePageUrl}${issueId}/user-reports/`
+      `${this.issuePageUrl}${issueId}/user-reports/`,
+      { observe: "response", params: httpParams }
     );
   }
 
@@ -80,10 +105,42 @@ export class UserReportsService {
     });
   }
 
+  private resetUserReports() {
+    this.userReportsState.next({
+      ...this.userReportsState.getValue(),
+      reports: initialState.reports,
+    });
+  }
+
   private resetErrorsReports() {
     this.userReportsState.next({
       ...this.userReportsState.getValue(),
       errors: initialState.errors,
+    });
+  }
+
+  /**
+   * Pagination info exists in a header, parse it out and store it.
+   * Anything with an actual link indicates it has results. This differs just
+   * very slightly from sentry open source.
+   */
+  private setPagination(linkHeader: string) {
+    const parts = linkHeader
+      .split(",")
+      .reduce<{ [key: string]: string }>((acc, link) => {
+        const match = link.match(/<(.*)>; rel="(\w*)"/);
+        if (match) {
+          const url = match[1];
+          const rel = match[2];
+          acc[rel] = url;
+          return acc;
+        }
+        return {};
+      }, {});
+    this.userReportsState.next({
+      ...this.userReportsState.getValue(),
+      nextPage: parts.next ? parts.next : null,
+      previousPage: parts.prev ? parts.prev : null,
     });
   }
 }
--- src/app/issues/user-reports-issue/user-reports-issue.component.ts
+++ src/app/issues/user-reports-issue/user-reports-issue.component.ts
@@ -1,7 +1,9 @@
-import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { Component, ChangeDetectionStrategy, OnDestroy } from "@angular/core";
 import { IssueDetailService } from "../issue-detail/issue-detail.service";
 import { UserReportsService } from "src/app/api/user-reports/user-reports.service";
 import { map, tap } from "rxjs/operators";
+import { Subscription, combineLatest } from "rxjs";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-user-reports-issue",
@@ -9,24 +11,43 @@ import { map, tap } from "rxjs/operators";
   styleUrls: ["./user-reports-issue.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserReportsIssueComponent {
+export class UserReportsIssueComponent implements OnDestroy {
   issueId$ = this.issueService.issue$.pipe(map((issue) => issue?.id));
   reports$ = this.userReportService.reports$;
   loadingReports$ = this.userReportService.loadingReports$;
   errorReports$ = this.userReportService.errorsReports$;
 
+  hasNextPage$ = this.userReportService.hasNextPage$;
+  hasPreviousPage$ = this.userReportService.hasPreviousPage$;
+  nextParams$ = this.userReportService.nextPageParams$;
+  previousParams$ = this.userReportService.previousPageParams$;
+
+  routerEventSubscription: Subscription;
+
   constructor(
     private issueService: IssueDetailService,
-    private userReportService: UserReportsService
+    private userReportService: UserReportsService,
+    private route: ActivatedRoute
   ) {
-    this.issueId$
+    this.routerEventSubscription = combineLatest([
+      this.route.queryParams,
+      this.issueId$,
+    ])
       .pipe(
-        tap((issueId) => {
+        map(([queryParams, issueId]) => {
+          const cursor: string | undefined = queryParams.cursor;
+          return { issueId, cursor };
+        }),
+        tap(({ issueId, cursor }) => {
           if (issueId) {
-            this.userReportService.getReportsForIssue(issueId);
+            this.userReportService.getReportsForIssue(issueId, cursor);
           }
         })
       )
       .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.routerEventSubscription.unsubscribe();
   }
 }
