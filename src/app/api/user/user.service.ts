f9d83f46e25e55f063dc761c586dcaeaffe4dfff
David Burke <david@burkesoftware.com>
2020-11-01 15:47:56 -0500

    Use exhaustMap to prevent double firing of getUserDetails

--- src/app/api/user/user.service.ts
+++ src/app/api/user/user.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { BehaviorSubject, EMPTY } from "rxjs";
-import { tap, map, catchError } from "rxjs/operators";
-import { User } from "./user.interfaces";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { BehaviorSubject, EMPTY, Subject } from "rxjs";
+import { tap, map, catchError, exhaustMap } from "rxjs/operators";
+import { User } from "./user.interfaces";
 
 interface UserState {
   user: User | null;
@@ -19,20 +19,31 @@ const initialState: UserState = {
 export class UserService {
   private readonly state = new BehaviorSubject<UserState>(initialState);
   readonly userDetails$ = this.state.pipe(map((state) => state.user));
+  private readonly getUserDetailsAction = new Subject();
 
   readonly activeUserEmail$ = this.userDetails$.pipe(
     map((userDetails) => userDetails?.email)
   );
   private readonly url = "/api/0/users/me/";
 
-  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {
+    this.getUserDetailsAction
+      .pipe(
+        exhaustMap(() =>
+          this.retrieveUserDetails().pipe(
+            tap((resp: User) => this.setUserDetails(resp)),
+            catchError(() => EMPTY)
+          )
+        )
+      )
+      .subscribe();
+  }
 
   /** Get and set current logged in user details from backend */
   getUserDetails() {
-    this.retrieveUserDetails()
-      .pipe(tap((resp: User) => this.setUserDetails(resp)))
-      .subscribe();
+    this.getUserDetailsAction.next();
   }
+
   private retrieveUserDetails() {
     return this.http.get<User>(this.url);
   }
