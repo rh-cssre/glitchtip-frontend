1bd6eb76c11ff6c079b712275ad56a0168ec2dec
Brendan <brendan@burkesoftware.com>
2020-12-04 17:45:47 -0500

    #112 tweaks to icon code, make story better

--- src/app/issues/issue-detail/event-detail/context/contexts.stories.ts
+++ src/app/issues/issue-detail/event-detail/context/contexts.stories.ts
@@ -2,13 +2,13 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { moduleMetadata } from "@storybook/angular";
-import { withKnobs, boolean, select } from "@storybook/addon-knobs";
+import { withKnobs, boolean } from "@storybook/addon-knobs";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { of } from "rxjs";
 
 import { SharedModule } from "../../../../shared/shared.module";
 import { ContextsComponent } from "./contexts.component";
-import { generateIconPath } from "src/app/shared/shared.utils";
+import { generateIconPath, iconDictionary } from "src/app/shared/shared.utils";
 
 export default {
   title: "Events/Contexts",
@@ -107,34 +107,26 @@ contexts.story = {
   name: "Event Detail Contexts",
 };
 
-const options = [
-  "chrome",
-  "firefox",
-  "opera",
-  "safari",
-  "mobilesafari",
-  "edge",
-  "chromium",
-  "chromemobile",
-  "chromemobileios",
-  "qqbrowser",
-  "playstation",
-  "internetexplorer",
-];
-const defaultValue = "chrome";
-
 export const IconPaths = () => {
-  const value = select("Path", options, defaultValue);
-  const path = generateIconPath(value);
-  const template = () => `
-    <div>
-      <p>path: ${path}</p>
-      <img class="image" src="${path}" />
+  const iconMarkup = Object.keys(iconDictionary).map(
+    (icon) => `
+    <div style="border: 1px solid lightgray; margin: 5px; padding: 0 10px 10px;">
+      <p style="font-size: 0.75em">${icon}</p>
+      <img
+        style="width: 50px; height: 50px;"
+        class="image"
+        src="${generateIconPath(icon)}"
+      />
     </div>
-  `;
+  `
+  );
 
   return {
-    template: template(),
+    template: `
+      <div style="display: flex; flex-wrap: wrap;">
+        ${iconMarkup.join("")}
+      </div>
+    `,
   };
 };
 
