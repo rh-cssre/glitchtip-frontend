7e01adc2cde39bde784bae451097b1ff265439c5
Brendan <brendan@burkesoftware.com>
2021-02-01 21:33:25 -0500

    fix test

--- src/app/shared/shared.utils.spec.ts
+++ src/app/shared/shared.utils.spec.ts
@@ -8,23 +8,23 @@ describe("Icon Paths", () => {
 
   it("generates path to os library", () => {
     const redHat = generateIconPath("Red Hat");
-    expect(redHat).toBe("assets/images/os-logos/red-hat.png");
+    expect(redHat).toBe("static/assets/images/os-logos/red-hat.png");
   });
 
   it("generates path to browser logo library", () => {
     const opera = generateIconPath("Opera");
-    expect(opera).toBe("assets/images/browser-svgs/opera/opera.svg");
+    expect(opera).toBe("static/assets/images/browser-svgs/opera/opera.svg");
   });
 
   it("generates path to browser logo archive library", () => {
     const internetExplorer = generateIconPath("Internet Explorer");
     expect(internetExplorer).toBe(
-      "assets/images/browser-svgs/archive/internet-explorer-tile_10-11/internet-explorer-tile_10-11.svg"
+      "static/assets/images/browser-svgs/archive/internet-explorer-tile_10-11/internet-explorer-tile_10-11.svg"
     );
   });
 
   it("generates path to local library", () => {
     const cPython = generateIconPath("C Python");
-    expect(cPython).toBe("assets/images/logos/48x48/cpython.png");
+    expect(cPython).toBe("static/assets/images/logos/48x48/cpython.png");
   });
 });
