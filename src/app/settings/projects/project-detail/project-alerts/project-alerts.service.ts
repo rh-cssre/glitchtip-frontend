cd3e94f7bb1f6d09413ca7d46ab67595c9d895f8
Emily Jensen <emily@burkesoftware.com>
2021-05-14 10:45:28 -0400

    Set project alert as array

--- src/app/settings/projects/project-detail/project-alerts/project-alerts.service.ts
+++ src/app/settings/projects/project-detail/project-alerts/project-alerts.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@angular/core";
-import { HttpErrorResponse } from "@angular/common/http";
-import { MatSnackBar } from "@angular/material/snack-bar";
+// import { HttpErrorResponse } from "@angular/common/http";
+// import { MatSnackBar } from "@angular/material/snack-bar";
 import { BehaviorSubject, combineLatest, EMPTY } from "rxjs";
 import {
   tap,
   map,
   mergeMap,
   take,
-  catchError,
-  exhaustMap,
+  // catchError,
+  // exhaustMap,
 } from "rxjs/operators";
 import { OrganizationsService } from "../../../../api/organizations/organizations.service";
 import { ProjectAlertsAPIService } from "../../../../api/projects/project-alerts/project-alerts.service";
@@ -16,7 +16,7 @@ import { ProjectAlert } from "../../../../api/projects/project-alerts/project-al
 import { ProjectSettingsService } from "../../project-settings.service";
 
 interface ProjectsState {
-  projectAlerts: ProjectAlert | null;
+  projectAlerts: ProjectAlert[] | null;
   alertToggleLoading: boolean;
   alertToggleError: string;
   initialLoad: boolean;
@@ -58,8 +58,7 @@ export class ProjectAlertsService {
   constructor(
     private organizationsService: OrganizationsService,
     private projectSettingsService: ProjectSettingsService,
-    private projectAlertsAPIService: ProjectAlertsAPIService,
-    private snackBar: MatSnackBar
+    private projectAlertsAPIService: ProjectAlertsAPIService // private snackBar: MatSnackBar
   ) {}
 
   listProjectAlerts() {
@@ -74,8 +73,8 @@ export class ProjectAlertsService {
             return this.projectAlertsAPIService.list(orgSlug, projectSlug).pipe(
               tap((projectAlerts) => {
                 if (projectAlerts.length) {
-                  /** Not supporting multiple project alerts yet */
-                  this.setActiveProjectAlert(projectAlerts[0]);
+                  console.log("project alerts: ", projectAlerts);
+                  this.setActiveProjectAlert(projectAlerts);
                 } else {
                   this.setActiveProjectAlert(null);
                 }
@@ -88,119 +87,119 @@ export class ProjectAlertsService {
       .subscribe();
   }
 
-  toggleProjectAlerts() {
-    this.setAlertToggleLoading(true);
-    combineLatest([
-      this.projectAlert$,
-      this.organizationsService.activeOrganizationSlug$,
-      this.projectSettingsService.activeProjectSlug$,
-    ])
-      .pipe(
-        take(1),
-        exhaustMap(([projectAlert, orgSlug, projectSlug]) => {
-          if (orgSlug && projectSlug) {
-            if (projectAlert) {
-              return this.projectAlertsAPIService
-                .destroy(projectAlert.pk.toString(), orgSlug, projectSlug)
-                .pipe(
-                  tap((_) => {
-                    this.listProjectAlerts();
-                    this.setAlertToggleLoading(false);
-                  }),
-                  catchError((error: HttpErrorResponse) => {
-                    this.setAlertToggleLoading(false);
-                    if (error) {
-                      this.setAlertToggleError(
-                        `${error.status}: ${error.name}/${error.statusText}`
-                      );
-                    }
-                    return EMPTY;
-                  })
-                );
-            } else {
-              const data = {
-                timespan_minutes: 1,
-                quantity: 1,
-              };
-              return this.projectAlertsAPIService
-                .create(data, orgSlug, projectSlug)
-                .pipe(
-                  tap((resp) => {
-                    this.setAlertToggleLoading(false);
-                    this.setActiveProjectAlert(resp);
-                  }),
-                  catchError((error: HttpErrorResponse) => {
-                    this.setAlertToggleLoading(false);
-                    if (error) {
-                      this.setError(
-                        `${error.status}: ${error.name}/${error.statusText}`
-                      );
-                    }
-                    return EMPTY;
-                  })
-                );
-            }
-          }
+  // toggleProjectAlerts() {
+  //   this.setAlertToggleLoading(true);
+  //   combineLatest([
+  //     this.projectAlert$,
+  //     this.organizationsService.activeOrganizationSlug$,
+  //     this.projectSettingsService.activeProjectSlug$,
+  //   ])
+  //     .pipe(
+  //       take(1),
+  //       exhaustMap(([projectAlert, orgSlug, projectSlug]) => {
+  //         if (orgSlug && projectSlug) {
+  //           if (projectAlert) {
+  //             return this.projectAlertsAPIService
+  //               .destroy(projectAlert.pk.toString(), orgSlug, projectSlug)
+  //               .pipe(
+  //                 tap((_) => {
+  //                   this.listProjectAlerts();
+  //                   this.setAlertToggleLoading(false);
+  //                 }),
+  //                 catchError((error: HttpErrorResponse) => {
+  //                   this.setAlertToggleLoading(false);
+  //                   if (error) {
+  //                     this.setAlertToggleError(
+  //                       `${error.status}: ${error.name}/${error.statusText}`
+  //                     );
+  //                   }
+  //                   return EMPTY;
+  //                 })
+  //               );
+  //           } else {
+  //             const data = {
+  //               timespan_minutes: 1,
+  //               quantity: 1,
+  //             };
+  //             return this.projectAlertsAPIService
+  //               .create(data, orgSlug, projectSlug)
+  //               .pipe(
+  //                 tap((resp) => {
+  //                   this.setAlertToggleLoading(false);
+  //                   this.setActiveProjectAlert(resp);
+  //                 }),
+  //                 catchError((error: HttpErrorResponse) => {
+  //                   this.setAlertToggleLoading(false);
+  //                   if (error) {
+  //                     this.setError(
+  //                       `${error.status}: ${error.name}/${error.statusText}`
+  //                     );
+  //                   }
+  //                   return EMPTY;
+  //                 })
+  //               );
+  //           }
+  //         }
 
-          return EMPTY;
-        })
-      )
-      .toPromise();
-  }
+  //         return EMPTY;
+  //       })
+  //     )
+  //     .toPromise();
+  // }
 
-  updateProjectAlerts(timespan: number, amount: number) {
-    this.setLoading(true);
-    combineLatest([
-      this.organizationsService.activeOrganizationSlug$,
-      this.projectSettingsService.activeProjectSlug$,
-      this.projectAlert$,
-    ])
-      .pipe(
-        take(1),
-        exhaustMap(([orgSlug, projectSlug, projectAlert]) => {
-          if (orgSlug && projectSlug) {
-            const data = {
-              timespan_minutes: timespan,
-              quantity: amount,
-            };
-            if (projectAlert) {
-              const pk = projectAlert?.pk.toString();
-              return this.projectAlertsAPIService
-                .update(pk, data, orgSlug, projectSlug)
-                .pipe(
-                  tap((resp) => {
-                    this.setLoading(false);
-                    this.setActiveProjectAlert(resp);
-                  }),
-                  catchError((error: HttpErrorResponse) => {
-                    this.setLoading(false);
-                    if (error.status === 404) {
-                      this.snackBar.open(`Error: ${error.statusText}`);
-                      this.setActiveProjectAlert(null);
-                    } else {
-                      this.setError(
-                        `${error.status}: ${error.name}/${error.statusText}`
-                      );
-                    }
-                    return EMPTY;
-                  })
-                );
-            } else {
-              this.toggleProjectAlerts();
-            }
-          }
+  // updateProjectAlerts(timespan: number, amount: number) {
+  //   this.setLoading(true);
+  //   combineLatest([
+  //     this.organizationsService.activeOrganizationSlug$,
+  //     this.projectSettingsService.activeProjectSlug$,
+  //     this.projectAlert$,
+  //   ])
+  //     .pipe(
+  //       take(1),
+  //       exhaustMap(([orgSlug, projectSlug, projectAlert]) => {
+  //         if (orgSlug && projectSlug) {
+  //           const data = {
+  //             timespan_minutes: timespan,
+  //             quantity: amount,
+  //           };
+  //           if (projectAlert) {
+  //             const pk = projectAlert?.pk.toString();
+  //             return this.projectAlertsAPIService
+  //               .update(pk, data, orgSlug, projectSlug)
+  //               .pipe(
+  //                 tap((resp) => {
+  //                   this.setLoading(false);
+  //                   this.setActiveProjectAlert(resp);
+  //                 }),
+  //                 catchError((error: HttpErrorResponse) => {
+  //                   this.setLoading(false);
+  //                   if (error.status === 404) {
+  //                     this.snackBar.open(`Error: ${error.statusText}`);
+  //                     this.setActiveProjectAlert(null);
+  //                   } else {
+  //                     this.setError(
+  //                       `${error.status}: ${error.name}/${error.statusText}`
+  //                     );
+  //                   }
+  //                   return EMPTY;
+  //                 })
+  //               );
+  //           } else {
+  //             this.toggleProjectAlerts();
+  //           }
+  //         }
 
-          return EMPTY;
-        })
-      )
-      .toPromise();
-  }
+  //         return EMPTY;
+  //       })
+  //     )
+  //     .toPromise();
+  // }
 
   clearState() {
     this.projectAlertState.next(initialState);
   }
 
-  private setActiveProjectAlert(alert: ProjectAlert | null) {
+  private setActiveProjectAlert(alert: ProjectAlert[] | null) {
     this.projectAlertState.next({
       ...this.projectAlertState.getValue(),
       projectAlerts: alert,
@@ -208,31 +207,31 @@ export class ProjectAlertsService {
     });
   }
 
-  private setAlertToggleLoading(isLoading: boolean) {
-    this.projectAlertState.next({
-      ...this.projectAlertState.getValue(),
-      alertToggleLoading: isLoading,
-    });
-  }
+  // private setAlertToggleLoading(isLoading: boolean) {
+  //   this.projectAlertState.next({
+  //     ...this.projectAlertState.getValue(),
+  //     alertToggleLoading: isLoading,
+  //   });
+  // }
 
-  private setAlertToggleError(error: string) {
-    this.projectAlertState.next({
-      ...this.projectAlertState.getValue(),
-      alertToggleError: error,
-    });
-  }
+  // private setAlertToggleError(error: string) {
+  //   this.projectAlertState.next({
+  //     ...this.projectAlertState.getValue(),
+  //     alertToggleError: error,
+  //   });
+  // }
 
-  private setLoading(isLoading: boolean) {
-    this.projectAlertState.next({
-      ...this.projectAlertState.getValue(),
-      loading: isLoading,
-    });
-  }
+  // private setLoading(isLoading: boolean) {
+  //   this.projectAlertState.next({
+  //     ...this.projectAlertState.getValue(),
+  //     loading: isLoading,
+  //   });
+  // }
 
-  private setError(err: string) {
-    this.projectAlertState.next({
-      ...this.projectAlertState.getValue(),
-      error: err,
-    });
-  }
+  // private setError(err: string) {
+  //   this.projectAlertState.next({
+  //     ...this.projectAlertState.getValue(),
+  //     error: err,
+  //   });
+  // }
 }
