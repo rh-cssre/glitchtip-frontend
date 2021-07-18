d6b64ef531ae4ef8fe722ecaf1d92fdb27e74c02
David Burke <david@burkesoftware.com>
2021-07-18 12:42:19 -0400

    Fix spec

--- src/app/profile/multi-factor-auth/multi-factor-auth.component.spec.ts
+++ src/app/profile/multi-factor-auth/multi-factor-auth.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { MultiFactorAuthComponent } from "./multi-factor-auth.component";
@@ -8,9 +9,9 @@ describe("MultiFactorAuthComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MultiFactorAuthComponent ]
-    })
-    .compileComponents();
+      declarations: [MultiFactorAuthComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
