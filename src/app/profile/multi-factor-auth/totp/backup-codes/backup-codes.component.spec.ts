6405be63dfb94342dad23c590711d47b9c5f27a7
James Kiger <james.kiger@gmail.com>
2021-09-14 12:23:30 -0400

    Backup codes refactored out of TOTP, WIP for regen

---
+++ src/app/profile/multi-factor-auth/totp/backup-codes/backup-codes.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BackupCodesComponent } from './backup-codes.component';
+
+describe('BackupCodesComponent', () => {
+  let component: BackupCodesComponent;
+  let fixture: ComponentFixture<BackupCodesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BackupCodesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BackupCodesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/profile/multi-factor-auth/totp/totp.component.ts
+++ src/app/profile/multi-factor-auth/totp/totp.component.ts
@@ -9,7 +9,7 @@ import {
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import * as QRCode from "qrcode";
 import { combineLatest } from "rxjs";
-import { delay, filter, take, tap } from "rxjs/operators";
+import { delay, filter, tap } from "rxjs/operators";
 import { MultiFactorAuthService } from "../multi-factor-auth.service";
 
 @Component({
@@ -24,7 +24,6 @@ export class TOTPComponent implements OnInit, OnDestroy {
   TOTP$ = this.service.totp$;
   step$ = this.service.setupTOTPStage$;
   error$ = this.service.serverError$;
-  copiedCodes$ = this.service.copiedCodes$;
   codeForm = new FormGroup({
     code: new FormControl("", [
       Validators.required,
@@ -32,22 +31,11 @@ export class TOTPComponent implements OnInit, OnDestroy {
       Validators.maxLength(6),
     ]),
   });
-  backupCodeForm = new FormGroup({
-    backupCode: new FormControl("", [
-      Validators.required,
-      Validators.minLength(16),
-      Validators.maxLength(16),
-    ]),
-  });
 
   constructor(private service: MultiFactorAuthService) {}
 
   get code() {
     return this.codeForm.get("code");
-  }
-
-  get backupCode() {
-    return this.backupCodeForm.get("backupCode");
   }
 
   ngOnInit() {
@@ -98,46 +86,5 @@ export class TOTPComponent implements OnInit, OnDestroy {
     if (this.canvas) {
       QRCode.toCanvas(this.canvas.nativeElement, value);
     }
-  }
-
-  copyCodes() {
-    this.service.backupCodes$.pipe(take(1)).subscribe((codes) => {
-      if (codes) {
-        navigator.clipboard.writeText(codes.join("\n"));
-        this.service.setCopiedCodes();
-      }
-    });
-  }
-
-  downloadCodes() {
-    this.service.backupCodes$.pipe(take(1)).subscribe((codes) => {
-      if (codes) {
-        this.download("glitchtip-backup.txt", codes.join("\n"));
-        this.service.setCopiedCodes();
-      }
-    });
-  }
-
-  verifyBackupCode() {
-    const code = this.backupCodeForm.get("backupCode")?.value;
-    if (this.backupCodeForm.valid && code) {
-      this.service.verifyBackupCode(code).subscribe();
-    }
-  }
-
-  private download(filename: string, text: string) {
-    const element = document.createElement("a");
-    element.setAttribute(
-      "href",
-      "data:text/plain;charset=utf-8," + encodeURIComponent(text)
-    );
-    element.setAttribute("download", filename);
-
-    element.style.display = "none";
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
   }
 }
