2973ba87f9e1b4450550031ffbd99755eac42c07
David Burke <david@burkesoftware.com>
2021-09-28 14:43:35 -0400

    Fix spec

--- src/app/profile/wizard/wizard.component.spec.ts
+++ src/app/profile/wizard/wizard.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
 import { WizardComponent } from "./wizard.component";
 
@@ -8,9 +10,9 @@ describe("WizardComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WizardComponent ]
-    })
-    .compileComponents();
+      declarations: [WizardComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
