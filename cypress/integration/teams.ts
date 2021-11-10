3292ad0fc183b078bd30ddc85cdba4b6faf32278
James Kiger <james.kiger@gmail.com>
2021-11-10 14:25:15 -0500

    Reverts cypress test back to original state since we are no longer chaning this component.

--- cypress/integration/teams.ts
+++ cypress/integration/teams.ts
@@ -20,8 +20,7 @@ describe("Create New Team", () => {
     cy.visit(`/${organization.slug}/settings/teams`);
     cy.get("#new-team").click();
     cy.get("input[formcontrolname=slug]").type(
-      newTeam.slug + " invalid ch@r@cter$",
-      {force: true}
+      newTeam.slug + " invalid ch@r@cter$"
     );
     cy.get("#create-team-submit").click();
     cy.contains("consisting of letters, numbers, underscores or hyphens.");
