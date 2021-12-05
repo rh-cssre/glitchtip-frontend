6daeb7242451ac867d03d4fc69eeada60baf94c5
David Burke <david@burkesoftware.com>
2021-12-05 10:38:41 -0500

    Related to https://gitlab.com/glitchtip/glitchtip/-/issues/26 Add option to delete user account.

--- src/app/profile/account/account.component.ts
+++ src/app/profile/account/account.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { EMPTY } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { AuthService } from "src/app/api/auth/auth.service";
+import { UserService } from "src/app/api/user/user.service";
 
 @Component({
   selector: "gt-account",
@@ -6,5 +11,34 @@ import { Component } from "@angular/core";
   styleUrls: ["./account.component.scss"],
 })
 export class AccountComponent {
-  constructor() {}
+  deleteLoading = false;
+  deleteError = "";
+
+  constructor(
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
+
+  deleteUser() {
+    if (
+      window.confirm(
+        `Are you sure you want to delete your user account? You will permanently lose access to all organizations, projects, and teams associated with it.`
+      )
+    ) {
+      this.deleteLoading = true;
+      this.userService
+        .deleteUser()
+        .pipe(
+          tap(() => this.authService.removeAuth()),
+          catchError((err) => {
+            this.deleteLoading = false;
+            if (err instanceof HttpErrorResponse) {
+              this.deleteError = "Unable to delete user";
+            }
+            return EMPTY;
+          })
+        )
+        .toPromise();
+    }
+  }
 }
