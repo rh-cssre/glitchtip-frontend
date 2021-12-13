7e1d37670f314063b18052cddac5e2cf54845f83
James Kiger <james.kiger@gmail.com>
2021-12-13 15:43:20 -0500

    Adds pipe for converting response time to seconds or ms.

---
+++ src/app/shared/seconds-or-ms.pipe.spec.ts
@@ -0,0 +1,21 @@
+import { SecondsOrMSPipe } from "./seconds-or-ms.pipe";
+
+fdescribe("SecondsOrMSPipe", () => {
+  const secondsOrMSPipe = new SecondsOrMSPipe
+
+  it("create an instance", () => {
+    expect(secondsOrMSPipe).toBeTruthy();
+  });
+
+  it("returns a time delta as either seconds or milliseconds", () => {
+    const msDuration = "0:00:00.063595"
+    const secondsDuration = "0:00:01.063595"
+
+    expect(secondsOrMSPipe.transform(msDuration)).toBe("64ms")
+    expect(secondsOrMSPipe.transform(secondsDuration)).toBe("1.06 seconds")
+  });
+
+  it("an invalid duration does not transform the value", () => {
+    expect(secondsOrMSPipe.transform("fake")).toBe("");
+  });
+});
