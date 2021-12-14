a3a7a9538a2303ed17ec4b57fc91e03d3a95be61
James Kiger <james.kiger@gmail.com>
2021-12-14 07:58:49 -0500

    Fixes seconds pipe to handle nuill values

--- src/app/shared/seconds-or-ms.pipe.ts
+++ src/app/shared/seconds-or-ms.pipe.ts
@@ -6,24 +6,27 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class SecondsOrMSPipe implements PipeTransform {
   transform(value: string): string {
     let seconds = 0;
-    if (value.includes(" ")) {
-      seconds += parseInt(value.split(" ")[0], 10) * 86400;
-      value = value.split(" ")[1];
-    }
-    const splitValue = value.split(":");
-    seconds += parseInt(splitValue[0], 10) * 3600;
-    seconds += parseInt(splitValue[1], 10) * 60;
-
-    let secondsAndMS = parseFloat(splitValue[2]);
-    if (isNaN(seconds) || isNaN(secondsAndMS)) {
-      return ""
-    } else {
-      if (secondsAndMS > 1) {
-        return `${(secondsAndMS + seconds).toFixed(2)} seconds`;
-      } else {
-        return `${Math.round(secondsAndMS * 1000)}ms`;
+    if (value) {
+      if (value.includes(" ")) {
+        seconds += parseInt(value.split(" ")[0], 10) * 86400;
+        value = value.split(" ")[1];
       }
+      const splitValue = value.split(":");
+      seconds += parseInt(splitValue[0], 10) * 3600;
+      seconds += parseInt(splitValue[1], 10) * 60;
+
+      let secondsAndMS = parseFloat(splitValue[2]);
+      if (isNaN(seconds) || isNaN(secondsAndMS)) {
+        return "";
+      } else {
+        if (secondsAndMS > 1) {
+          return `${(secondsAndMS + seconds).toFixed(2)} seconds`;
+        } else {
+          return `${Math.round(secondsAndMS * 1000)}ms`;
+        }
+      }
+    } else {
+      return "";
     }
   }
 }
-
