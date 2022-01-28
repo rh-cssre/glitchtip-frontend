798ce9117b1a27ede1bc1a3a812c16577a3d6fcf
James Kiger <james.kiger@gmail.com>
2022-01-28 13:23:00 -0500

    Removes unnecessary interfaces.

--- src/app/api/oauth/oauth.interfaces.ts
+++ src/app/api/oauth/oauth.interfaces.ts
@@ -1,7 +1,3 @@
 /** Name as defined by django-allauth */
 export type OAuthProvider = "google" | "gitlab" | "github" | "microsoft";
 
-export interface OAuthConfig {
-  scope: string;
-  name: OAuthProvider;
-}
