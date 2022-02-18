0acdcbbe719a8672883a475129ee2b442fb10f6c
James Kiger <james.kiger@gmail.com>
2022-02-18 11:05:45 -0500

    Adds stub components to avoid errors during testing

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,3 +1,4 @@
+import { Component, Input } from "@angular/core";
 import { waitForAsync, ComponentFixture, TestBed } from "@angular/core/testing";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
@@ -7,6 +8,11 @@ import { EMPTY } from "rxjs";
 import { LoginService } from "./login.service";
 import { LoginComponent } from "./login.component";
 import { MaterialModule } from "../shared/material.module";
+
+@Component({selector: 'gt-form-error', template: ''})
+class FormErrorStub {
+  @Input() error: any;
+}
 
 // Type is wrong abouit createSpyObject, third param is for properties
 const authServiceSpy = (jasmine as any).createSpyObj(
@@ -22,7 +28,7 @@ describe("LoginComponent", () => {
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        declarations: [LoginComponent],
+        declarations: [LoginComponent, FormErrorStub],
         imports: [
           NoopAnimationsModule,
           ReactiveFormsModule,
