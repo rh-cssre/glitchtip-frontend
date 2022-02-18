6875485e5300ab4362ecb41c9420548c4d9bb5b2
James Kiger <james.kiger@gmail.com>
2022-02-18 12:03:08 -0500

    Removes non-failing type error from org test

--- src/app/api/organizations/organization-test-data.ts
+++ src/app/api/organizations/organization-test-data.ts
@@ -1,4 +1,16 @@
-import { Organization } from "./organizations.interface";
+import {
+  Organization,
+  OrganizationProject,
+  OrganizationDetail,
+} from "./organizations.interface";
+
+interface APIOrganizationProject extends Omit<OrganizationProject, "id"> {
+  id: string;
+}
+
+interface APIOrganizationDetail extends Omit<OrganizationDetail, "projects"> {
+  projects: APIOrganizationProject[];
+}
 
 export const organizationList: Organization[] = [
   {
@@ -36,3 +48,46 @@ export const organizationList: Organization[] = [
     isAcceptingEvents: true,
   },
 ];
+
+export const testOrgDetail: APIOrganizationDetail = {
+  id: 1,
+  name: "prince ali",
+  slug: "prince-ali",
+  dateCreated: "2019-12-20T14:49:28.631459Z",
+  status: {
+    id: "active",
+    name: "active",
+  },
+  avatar: {
+    avatarType: "",
+    avatarUuid: null,
+  },
+  isEarlyAdopter: false,
+  require2FA: false,
+  isAcceptingEvents: true,
+  projects: [
+    {
+      platform: "",
+      slug: "uptime-test",
+      id: "72",
+      name: "Uptime Test",
+      isMember: true,
+    },
+  ],
+  teams: [
+    {
+      dateCreated: "2022-02-16T16:54:36.086069Z",
+      id: 38,
+      isMember: true,
+      memberCount: 1,
+      slug: "test2",
+    },
+    {
+      dateCreated: "2022-02-07T16:54:57.158485Z",
+      id: 37,
+      isMember: true,
+      memberCount: 1,
+      slug: "test",
+    },
+  ],
+};
