da3906652c25b634303fb5c8e579c3d47b66ef55
Deepak Sharma <deepshar@redhat.com>
2022-03-15 11:56:17 +0530

    adding Integration Pipeline

---
+++ pr_check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This Job Runs End to End Tests
+
+# TODO
+# 1. Run the backend server. From the backend repo run `docker-compose up`. Do not enable Stripe.
+# 2. Run the frontend development server `npm start`
+# 3. Run Cypress `npm run cy:open`
+
+echo "Skipped running Tests."
