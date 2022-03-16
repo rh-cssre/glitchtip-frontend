27f8f3884badbec0f62051a49a5c92c943ccc183
James Kiger <james.kiger@gmail.com>
2022-03-16 14:07:59 -0400

    Removes storybook knobs add on

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,6 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-knobs",
     "@storybook/addon-actions",
     "@storybook/addon-a11y",
   ],
