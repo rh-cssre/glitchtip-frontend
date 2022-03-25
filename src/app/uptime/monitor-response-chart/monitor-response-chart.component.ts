85d9a08190c86beb629693649d7101871c27ec76
James Kiger <james.kiger@gmail.com>
2022-03-25 13:00:45 -0400

    Comments on workarounds and resorting imports

--- src/app/uptime/monitor-response-chart/monitor-response-chart.component.ts
+++ src/app/uptime/monitor-response-chart/monitor-response-chart.component.ts
@@ -28,18 +28,23 @@ export class MonitorResponseChartComponent
   };
 
   @ViewChild("containerRef") containerRef?: ElementRef;
+  //Ngx-charts does not do continuous resizing, but only adjusts after window resizing completes. 
+  //This is a workaround to force continous resizing
   @HostListener("window:resize")
   windowResize() {
     this.resizeChart();
   }
 
   delayedResize$?: Subscription;
+  //Keeping these dimensions in state to avoid ngx-chart bleeding off edge of mat-card
   view: [number, number] = [0, 0];
   customColors = [
     { name: "Up", value: "#54a65a" },
     { name: "Down", value: "#e22a46" },
   ];
 
+  //Workaround to resize chart after main-nav closes or opens
+  //Change detector is needed to force Angular to recognize the change being made in the subscription
   constructor(private changeDetector: ChangeDetectorRef) {}
   ngOnInit(): void {
     this.delayedResize$ = fromEvent(window, "resize")
--- src/app/uptime/uptime.module.ts
+++ src/app/uptime/uptime.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { UptimeRoutingModule } from "./uptime-routing.module";
-import { MonitorListComponent } from "./monitor-list/monitor-list.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatTableModule } from "@angular/material/table";
-import { MaterialModule } from "../shared/material.module";
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { SharedModule } from "../shared/shared.module";
-import { NewMonitorComponent } from "./new-monitor/new-monitor.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MonitorDetailComponent } from "./monitor-detail/monitor-detail.component";
-import { MonitorUpdateComponent } from "./monitor-update/monitor-update.component";
-import { MonitorChecksComponent } from "./monitor-checks/monitor-checks.component";
+
 import { MonitorChartComponent } from './monitor-chart/monitor-chart.component';
+import { MonitorChecksComponent } from "./monitor-checks/monitor-checks.component";
+import { MonitorDetailComponent } from "./monitor-detail/monitor-detail.component";
+import { MonitorListComponent } from "./monitor-list/monitor-list.component";
 import { MonitorResponseChartComponent } from './monitor-response-chart/monitor-response-chart.component';
+import { MonitorUpdateComponent } from "./monitor-update/monitor-update.component";
+import { NewMonitorComponent } from "./new-monitor/new-monitor.component";
+import { MaterialModule } from "../shared/material.module";
+import { SharedModule } from "../shared/shared.module";
+import { UptimeRoutingModule } from "./uptime-routing.module";
 
 @NgModule({
   declarations: [
