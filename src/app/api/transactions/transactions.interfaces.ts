b664536407e112511c8fd8271fd012e46c4ca054
James Kiger <james.kiger@gmail.com>
2022-04-01 08:58:17 -0400

    Adds transaction group and span services.

--- src/app/api/transactions/transactions.interfaces.ts
+++ src/app/api/transactions/transactions.interfaces.ts
@@ -1,8 +1,34 @@
-export interface Transaction {
-  eventId: string;
+export interface Span {
+  spanId: string;
+  parentSpanId: string;
+  op: string;
+  description: string;
+  startTimestamp: string;
+  timestamp: string;
+  tags?: { [key: string]: string | null } | null;
+  data?: { [key: string]: string | null } | null;
+}
+
+export interface TransactionBase {
   transaction: string;
+  op: string;
+  method: string | null;
+}
+
+export interface Transaction extends TransactionBase {
+  eventId: string;
   timestamp: string;
   startTimestamp: string;
+}
+
+export interface TransactionDetail extends TransactionBase {
+  spans: Span[]
+}
+
+export interface TransactionGroup extends TransactionBase{
+  project: number;
+  avgDuration: number;
+  transactionCount: number;
 }
 
 export interface TransactionWithDelta extends Transaction {
