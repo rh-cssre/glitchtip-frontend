b7667a3f711ecc0b72e1edfe244758ece49bef5d
James Kiger <james.kiger@gmail.com>
2022-04-07 12:31:10 -0400

    Import sort

--- src/app/list-elements/list-elements.module.ts
+++ src/app/list-elements/list-elements.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
 import { MatDatepickerModule } from "@angular/material/datepicker";
+import { ReactiveFormsModule } from "@angular/forms";
 import { SharedModule } from "../shared/shared.module";
 import { ListActionsComponent } from "./list-actions/list-actions.component";
-import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
     MatDatepickerModule,
-    SharedModule,
     ReactiveFormsModule,
+    SharedModule,
   ],
   declarations: [ListActionsComponent],
   exports: [ListActionsComponent],
