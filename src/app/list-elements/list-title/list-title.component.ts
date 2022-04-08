73c6408afc551286228be80dbd6ee52d3b030eea
James Kiger <james.kiger@gmail.com>
2022-04-08 11:15:04 -0400

    Add list title component to uptime

--- src/app/list-elements/list-title/list-title.component.ts
+++ src/app/list-elements/list-title/list-title.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from "@angular/core";
 @Component({
   selector: "gt-list-title",
   templateUrl: "./list-title.component.html",
+  styleUrls: ["./list-title.component.scss"],
 })
 export class ListTitleComponent {
   @Input() searchHits?: string;
