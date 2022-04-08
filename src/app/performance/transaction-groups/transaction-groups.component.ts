45196bf3b646529fdb29c9d568273aac864d91ac
James Kiger <james.kiger@gmail.com>
2022-04-08 14:01:49 -0400

    Adds draft transaction group component

---
+++ src/app/performance/transaction-groups/transaction-groups.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+
+@Component({
+  selector: 'gt-transaction-groups',
+  templateUrl: './transaction-groups.component.html',
+  styleUrls: ['./transaction-groups.component.scss']
+})
+export class TransactionGroupsComponent {
+
+
+  sortForm = new FormGroup({
+    sort: new FormControl({
+      value: "",
+      disabled: true,
+    }),
+  });
+  dateForm = new FormGroup({
+    startDate: new FormControl(""),
+    endDate: new FormControl(""),
+  });
+
+  sorts = {
+    "-last_seen": "Last Seen",
+    last_seen: "First Seen",
+    "-created": "Newest Creation Date",
+    created: "Oldest Creation Date",
+    "-count": "Most Frequent",
+    count: "Least Frequent",
+    "-priority": "Highest Priority",
+    priority: "Lowest Priority",
+  };
+
+}
