53264bcfece16bb6322f72d4a95cebc6c8cdef69
James Kiger <james.kiger@gmail.com>
2022-04-12 14:34:41 -0400

    Gets params working.

--- src/app/performance/transaction-groups/transaction-groups.component.ts
+++ src/app/performance/transaction-groups/transaction-groups.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { FormControl, FormGroup } from "@angular/forms";
-import { withLatestFrom, Subscription, lastValueFrom } from "rxjs";
-import { map, tap } from "rxjs/operators";
+import { MatSelectChange } from "@angular/material/select";
+import { withLatestFrom, Subscription } from "rxjs";
+import { map } from "rxjs/operators";
 import { PerformanceService, PerformanceState } from "../performance.service";
 import { PaginationBaseComponent } from "src/app/shared/stateful-service/pagination-base.component";
-import { checkForOverflow, normalizeProjectParams } from "src/app/shared/shared.utils";
+import {
+  checkForOverflow,
+  normalizeProjectParams,
+} from "src/app/shared/shared.utils";
 
 @Component({
   selector: "gt-transaction-groups",
@@ -16,7 +20,7 @@ export class TransactionGroupsComponent
   extends PaginationBaseComponent<PerformanceState, PerformanceService>
   implements OnInit
 {
-  displayedColumns = ["name-and-project", "avgDuration"]
+  displayedColumns = ["name-and-project", "avgDuration"];
   sortForm = new FormGroup({
     sort: new FormControl({
       value: "",
@@ -29,14 +33,14 @@ export class TransactionGroupsComponent
   });
 
   sorts = {
-    "-avg_duration": "Feastest",
-    avg_duration: "Slowest",
+    "-avg_duration": "Slowest",
+    avg_duration: "Fastest",
     "-created": "Newest Creation Date",
     created: "Oldest Creation Date",
     "-transaction_count": "Most Frequent",
     transaction_count: "Least Frequent",
   };
-  tooltipDisabled = false
+  tooltipDisabled = false;
   transactionCountPluralMapping: { [k: string]: string } = {
     "=1": "1 Transaction",
     other: "# Transactions",
@@ -96,6 +100,12 @@ export class TransactionGroupsComponent
         }
       }
     );
+
+    this.transactionGroupsDisplay$.subscribe((groups) =>
+      groups.length === 0
+        ? this.sortForm.controls.sort.disable()
+        : this.sortForm.controls.sort.enable()
+    );
   }
 
   checkIfTooltipIsNecessary($event: Event) {
@@ -103,6 +113,43 @@ export class TransactionGroupsComponent
   }
 
   ngOnInit() {
-    lastValueFrom(this.transactionGroupsDisplay$.pipe(tap((groups) => console.log(groups))))
+    this.route.params.subscribe((_) => {
+      const start: string | undefined = this.route.snapshot.queryParams.start;
+      const end: string | undefined = this.route.snapshot.queryParams.end;
+      const sort: string | undefined = this.route.snapshot.queryParams.sort;
+      this.sortForm.setValue({
+        sort: sort !== undefined ? sort : "created",
+      });
+      this.dateForm.setValue({
+        startDate: start ? start : null,
+        endDate: end ? end : null,
+      });
+    });
+  }
+
+  onDateFormSubmit(queryParams: object) {
+    this.router.navigate([], {
+      queryParams,
+      queryParamsHandling: "merge",
+    });
+  }
+
+  dateFormReset() {
+    this.router.navigate([], {
+      queryParams: {
+        cursor: null,
+        start: null,
+        end: null,
+      },
+      queryParamsHandling: "merge",
+    });
+    this.dateForm.setValue({ startDate: null, endDate: null });
+  }
+
+  sortByChanged(event: MatSelectChange) {
+    this.router.navigate([], {
+      queryParams: { sort: event.value },
+      queryParamsHandling: "merge",
+    });
   }
 }
