0567d709a87b6d5a2707f595f349db6d676fd650
James Kiger <james.kiger@gmail.com>
2023-09-26 14:28:12 -0400

    Switch out package location

--- src/app/custom-microsentry-error-handler.ts
+++ src/app/custom-microsentry-error-handler.ts
@@ -1,6 +1,6 @@
 import { ErrorHandler, Injectable } from "@angular/core";
-import { MicroSentryErrorBusService } from "@micro-sentry/angular";
-import { MicroSentryService } from "@micro-sentry/angular";
+import { MicroSentryErrorBusService } from "@glitchtip/micro-sentry-angular";
+import { MicroSentryService } from "@glitchtip/micro-sentry-angular";
 
 @Injectable({ providedIn: "root" })
 export class CustomMicroSentryErrorHandler implements ErrorHandler {
